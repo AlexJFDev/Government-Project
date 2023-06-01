7dab66b1f97cab408929e8cb66d69cd3691ee034
AlexJFDev <alexjfuhr@gmail.com>
2023-06-01 09:51:58 -0400

    Added an in text citation component

---
+++ src/components/InTextCitation.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link to="/citations" class="citation">
+    {{ number }}
+    <v-tooltip
+      activator="parent"
+    >{{ source }}</v-tooltip>
+  </router-link>
+</template>
+<script>
+  export default {
+    name: 'InTextCitation',
+    props: {
+      source: String,
+      number: String,
+    }
+  }
+</script>
+<style>
+  .citation {
+    color: #2c3e50;
+    font-size: small;
+    text-decoration: none;
+  }
+</style>
